843e8197c721deb977682bc3843ce9d82f5cb3d8
PactForge <pactforge@gmail.com>
2025-04-20 18:29:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,6 +209,5 @@ def send_message():
 
         return jsonify({'response': response.text})
 
-
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+# The following lines should be removed or commented out for Render deployment
+# port = int(os.environ.get('PORT', 5000))
